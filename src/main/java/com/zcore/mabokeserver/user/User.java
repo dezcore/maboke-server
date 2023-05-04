938a1990dc6773f86d8517542c66275a49edc91d
Dez <dez@MacBook-Pro-de-Dez.local>
2023-05-04 13:20:56 +0200

    Init mysql env

--- src/main/java/com/zcore/mabokeserver/user/User.java
+++ src/main/java/com/zcore/mabokeserver/user/User.java
@@ -1,16 +1,23 @@
 package com.zcore.mabokeserver.user;
 
 import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-import org.springframework.data.annotation.Id;
-
+@Entity
 public class User {
     @Id
-    private Long id;
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
     private String name;
     private String email;
     private String password;
     private Date created_at;
     private Date updated_at;
     private String last_login_at;
+
+    
+    
 }
--- src/main/java/com/zcore/mabokeserver/user/UserRepository.java
+++ src/main/java/com/zcore/mabokeserver/user/UserRepository.java
@@ -2,6 +2,6 @@ package com.zcore.mabokeserver.user;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Integer> {
     
 }
